4aaa191a27df66fe3fb2105fd35615f8d382e164
Charly Ramirez <pingestudio@gmail.com>
2022-01-30 02:39:23 -0600

    Cors y useGlobalPipes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+
+const logger: Logger = new Logger('FerreSmart Cloud');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  // Pipes
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
+
   await app.listen(process.env.PORT);
+  logger.log(
+    `Micro Servicio: ${process.env.MICROSERVICIO}::${process.env.HOST}:${process.env.PORT}`,
+  );
 }
 bootstrap();
