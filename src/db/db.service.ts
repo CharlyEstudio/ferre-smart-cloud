19faf21e6c58e6f26d643b12502dc4daa9457666
Charly Ramirez <pingestudio@gmail.com>
2022-01-30 02:34:10 -0600

    Usando Environments Service para configurar DB

--- src/db/db.service.ts
+++ src/db/db.service.ts
@@ -1,25 +1,28 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConnectionOptions } from 'typeorm';
+import { EnvironmentsModel } from '@app/globals/environments/environments.model';
+import { EnvironmentService } from '@app/globals/environments/environment.service';
+import { Keys } from '@app/globals/environments/enums/keys';
 
 export const dbService = [
   TypeOrmModule.forRootAsync({
-    imports: [],
-    inject: [],
-    async useFactory(config: any) {
+    imports: [EnvironmentsModel],
+    inject: [EnvironmentService],
+    async useFactory(config: EnvironmentService) {
       return {
         timezone: 'Z',
         trace: true,
-        logging: false,
+        logging: config.get(Keys.TYPEORM_LOGGING),
         ssl: false,
-        type: 'mysql',
-        host: 'localhost',
-        port: 3306,
-        username: 'root',
-        password: '12345',
-        database: 'ferresmart',
+        type: config.get(Keys.TYPEORM_CONNECTION),
+        host: config.get(Keys.DATABASE_HOST),
+        port: +config.get(Keys.DATABASE_PORT),
+        username: config.get(Keys.DATABASE_USER),
+        password: config.get(Keys.DATABASE_PASSWORD),
+        database: config.get(Keys.DATABASE_NAME),
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         migrations: [__dirname + '/migrations/*{.ts,.js}'],
-        synchronize: false,
+        synchronize: config.get(Keys.TYPEORM_SYNCHRONIZE) === 'true',
         keepConnectionAlive: true,
       } as ConnectionOptions;
     },
