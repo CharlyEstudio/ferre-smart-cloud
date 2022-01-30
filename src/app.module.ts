9c50425abef11216f718845bb564a6357559aba4
Charly Ramirez <pingestudio@gmail.com>
2022-01-30 03:27:20 -0600

    Health DB Check

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './core/users/users.module';
 import { DbModule } from '@app/db/db.module';
 import { AuthModule } from './core/auth/auth.module';
+import { TerminusModule } from '@nestjs/terminus';
+import { HealthController } from './health/health.controller';
 
 @Module({
   imports: [
@@ -10,10 +12,11 @@ import { AuthModule } from './core/auth/auth.module';
       isGlobal: true,
     }),
     DbModule,
+    TerminusModule,
     UsersModule,
     AuthModule,
   ],
-  controllers: [],
+  controllers: [HealthController],
   providers: [],
 })
 export class AppModule {}
